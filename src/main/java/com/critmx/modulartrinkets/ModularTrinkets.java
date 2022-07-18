e9a3713fe8ffe6a5fab5226785a03efa74a2c6fd
CriTMx <savefailedmc@gmail.com>
2022-07-18 18:05:09 +0530

    refactors

--- src/main/java/com/critmx/modulartrinkets/ModularTrinkets.java
+++ src/main/java/com/critmx/modulartrinkets/ModularTrinkets.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.critmx.modulartrinkets;
 
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
@@ -18,13 +18,13 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
+@Mod("modulartrinkets")
+public class ModularTrinkets
 {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public ExampleMod() {
+    public ModularTrinkets() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
