91c2ec1163cd9015841850c9bcf75613471f7260
CriTMx <savefailedmc@gmail.com>
2022-07-18 17:29:46 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+
+public class Main
+{
+    final static Scanner sc = new Scanner(System.in);
+
+    static
+    {
+        
+    }
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }    
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# modulartrinkets
+ A repo for a Minecraft Mod I'm working on, called Modular Trinkets. Highly W.I.P. at the moment.
