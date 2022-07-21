531c04ba1e53cb5be43bdf467f9e4009468be16e
CriTMx <savefailedmc@gmail.com>
2022-07-21 18:40:07 +0530

    changes made

--- src/main/java/com/critmx/modulartrinkets/common/items/CrownBase.java
+++ src/main/java/com/critmx/modulartrinkets/common/items/CrownBase.java
@@ -6,6 +6,7 @@ import net.minecraftforge.registries.RegistryObject;
 public class CrownBase extends ItemRegister {
 
     public static final RegistryObject<Item>
-            CROWNBASE = ITEMS.register("crownbase", ()->new Item(new Item.Properties().tab(MTCreativeTab.MT_TAB)));
+            CROWNBASE = ITEMS.register("crownbase", ()->new Item(new Item.Properties().tab(MTCreativeTab.MT_TAB).stacksTo(1)));
+
 
 }
