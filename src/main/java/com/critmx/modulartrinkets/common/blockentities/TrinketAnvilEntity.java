bcae3c78ec7767fc4bb67e98c5d80d04d27935af
CriTMx <savefailedmc@gmail.com>
2022-07-21 18:08:22 +0530

    changes made

--- src/main/java/com/critmx/modulartrinkets/common/blockentities/TrinketAnvilEntity.java
+++ src/main/java/com/critmx/modulartrinkets/common/blockentities/TrinketAnvilEntity.java
@@ -1,16 +1,83 @@
 package com.critmx.modulartrinkets.common.blockentities;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.annotation.Nonnull;
 
 import static com.critmx.modulartrinkets.common.blockentities.BlockEntityRegister.TRINKETANVIL_BE;
 
-public class TrinketAnvilEntity extends BlockEntity {
+public class TrinketAnvilEntity extends BlockEntity implements MenuProvider {
 
-    public TrinketAnvilEntity(BlockPos pos, BlockState state)
+    private final ItemStackHandler itemHandler = new ItemStackHandler(7)
     {
-        super(TRINKETANVIL_BE.get(),pos,state);
+        @Override
+        protected void onContentsChanged(int slot)
+        {
+            setChanged();
+        }
+    };
+    private LazyOptional<IItemHandler> lazyItemHandler = LazyOptional.empty();
+
+    public TrinketAnvilEntity(BlockEntityType<?> pType, BlockPos pWorldPosition, BlockState pBlockState)
+    {
+        super(pType, pWorldPosition, pBlockState);
     }
+
+
+
+    @Override
+    public Component getDisplayName() {
+        return new TextComponent("Trinket Anvil");
+    }
+
+    @Nullable
+    @Override
+    public AbstractContainerMenu createMenu(int pContainerId, Inventory pInventory, Player pPlayer) {
+        return null;
+    }
+
+    @Nonnull
+    @Override
+    public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @javax.annotation.Nullable Direction side)
+    {
+        if (cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY)
+        {
+            return lazyItemHandler.cast();
+        }
+        return super.getCapability(cap, side);
+    }
+
+    @Override
+    public void invalidateCaps()
+    {
+        super.invalidateCaps();
+        lazyItemHandler.invalidate();
+    }
+
+    @Override
+    protected void saveAdditional(@NotNull CompoundTag tag)
+    {
+        tag.put("inventory",itemHandler.serializeNBT());
+        super.saveAdditional(tag);
+    }
+
 
 }
