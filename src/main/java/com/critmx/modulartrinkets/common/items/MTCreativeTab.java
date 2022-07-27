760dbbbb1b327eda802c6813571b0b209d523a15
CriTMx <savefailedmc@gmail.com>
2022-07-27 19:37:12 +0530

    changes made

--- src/main/java/com/critmx/modulartrinkets/common/items/MTCreativeTab.java
+++ src/main/java/com/critmx/modulartrinkets/common/items/MTCreativeTab.java
@@ -10,7 +10,7 @@ public class MTCreativeTab
     public static final CreativeModeTab MT_TAB = new CreativeModeTab("mtcreativetab") {
         @Override
         public ItemStack makeIcon() {
-            return new ItemStack(ItemRegister.CROWNBASE.get());
+            return new ItemStack(CrownBase.CROWNBASE.get());
         }
     };
 
