b3a1baad5a7e896d5a0dc784c51c02c42dbb0630
CriTMx <savefailedmc@gmail.com>
2022-12-08 21:58:20 +0530

    changes made

--- src/main/java/com/critmx/modulartrinkets/common/blocks/TrinketAnvil.java
+++ src/main/java/com/critmx/modulartrinkets/common/blocks/TrinketAnvil.java
@@ -2,16 +2,37 @@ package com.critmx.modulartrinkets.common.blocks;
 
 import com.critmx.modulartrinkets.common.deferredregisters.BlockRegister;
 import com.critmx.modulartrinkets.common.items.MTCreativeTab;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import net.minecraftforge.registries.RegistryObject;
 
-public class TrinketAnvil extends BlockRegister
+public final class TrinketAnvil extends Block
 {
+    String name;
+    Properties properties;
+    CreativeModeTab tab;
+    public static final TrinketAnvil TRINKETANVIL = new TrinketAnvil();
+    public TrinketAnvil()
+    {
+        super(BlockBehaviour.Properties.of(Material.METAL).strength(0.3f).requiresCorrectToolForDrops().dynamicShape());
+        properties = Properties.of(Material.METAL).strength(0.3f).requiresCorrectToolForDrops().dynamicShape();
+        name = "trinketanvil";
+        tab = MTCreativeTab.MT_TAB;
+    }
 
-
-
-
+    public String mt_getName()
+    {
+        return name;
+    }
+    public Properties mt_getProperties()
+    {
+        return properties;
+    }
+    public CreativeModeTab mt_getTab()
+    {
+        return tab;
+    }
 }
--- src/main/java/com/critmx/modulartrinkets/common/deferredregisters/BlockRegister.java
+++ src/main/java/com/critmx/modulartrinkets/common/deferredregisters/BlockRegister.java
@@ -1,6 +1,7 @@
 package com.critmx.modulartrinkets.common.deferredregisters;
 
 import com.critmx.modulartrinkets.ModularTrinkets;
+import com.critmx.modulartrinkets.common.blocks.TrinketAnvil;
 import com.critmx.modulartrinkets.common.deferredregisters.ItemRegister;
 import com.critmx.modulartrinkets.common.items.MTCreativeTab;
 import net.minecraft.world.item.BlockItem;
@@ -22,10 +23,13 @@ public class BlockRegister {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, ModularTrinkets.MOD_ID);
 
 
-    public static final RegistryObject<Block> TRINKETANVIL = registerBlock
+    /*public static final RegistryObject<Block> TRINKETANVIL = registerBlock
 
-            ("trinketanvil", ()->new Block(BlockBehaviour.Properties.copy(Blocks.GLASS).noOcclusion().of(Material.METAL).strength(0.3f).requiresCorrectToolForDrops().dynamicShape()),
-                    MTCreativeTab.MT_TAB);
+            ("trinketanvil", ()->new Block(BlockBehaviour.Properties.of(Material.METAL).strength(0.3f).requiresCorrectToolForDrops().dynamicShape()),
+                    MTCreativeTab.MT_TAB);*/
+
+    public static final RegistryObject<Block> TRINKETANVIL = registerBlock
+            (TrinketAnvil.TRINKETANVIL.mt_getName(),()->new Block(TrinketAnvil.TRINKETANVIL.mt_getProperties()),TrinketAnvil.TRINKETANVIL.mt_getTab());
 
     public static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab CreativeTab)
     {
